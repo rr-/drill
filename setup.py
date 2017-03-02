81e23f147300b7df32e86d3d7552576f65a68928
rr- <rr-@sakuya.pl>
2017-03-03 00:03:12 +0100

    Setup: add extra metadata

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
+    author='Marcin Kurczewski',
+    author_email='rr-@sakuya.pl',
     name='drillsrs',
+    long_description='Spaced repetition learning in CLI',
     version='0.1',
-    long_description=__doc__,
+    url='https://github.com/rr-/drill',
     packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
+
     entry_points={
         'console_scripts': [
             'drill-srs = drillsrs.__main__:main'
         ]
     },
+
     install_requires=[
         'python-dateutil',
         'sqlalchemy',
@@ -19,4 +22,16 @@ setup(
     ],
     package_dir={'drillsrs': 'drillsrs'},
     package_data={'drillsrs': ['data/*.*']},
-)
+    include_package_data=True,
+    zip_safe=False,
+
+    classifiers=[
+        'Environment :: Console',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Education',
+    ])
